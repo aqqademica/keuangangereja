caa30c9e5df3916280b66d9d0eb2cb20a7f858fb
aqqademica <aqqademica@gmail.com>
2026-06-13 12:21:06 +0700

    Untrack development planning documentation and SQL scratch scripts, keeping them local

--- gerejamanagement.md
+++
@@ -1,3 +0,0 @@
-supabase db pw:
-(G8j643.H8Hgq}[^5#
-(G8j643.H8Hgq}[^5#
--- scratch_update.sql
+++
@@ -1,9 +0,0 @@
-USE gereja_db;
-DELETE FROM user_security_answers;
-DELETE FROM security_questions;
-ALTER TABLE security_questions AUTO_INCREMENT = 1;
-
-INSERT INTO security_questions (question_text) VALUES 
-('Ayat Alkitab Favorit anda (namakitab:pasal:ayat) (*tanpa spasi)'),
-('Pastor / Pendeta / Penghkhotbah / Gembala Favorit Anda'),
-('Tokoh Alkitab yang anda Kagumi selain selain Yesus, Murid Yesus, dan Nabi-nabi.');
